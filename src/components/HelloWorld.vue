c96fd7bff85a17ce9d3e8886c92b45072d0646c9
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-12-05 12:20:38 +0200

    initial commit + bootstrap install

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="hello">
+    <div class="container">
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+    </div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 createApp(App).use(store).use(router).mount("#app");
